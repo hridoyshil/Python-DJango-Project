b1e918256cb2f40d614ee7dacd762d3bc3652a66
Hridoy <hriodykumarshil12@gmail.com>
2023-09-22 23:21:38 +0600

    Function Based Views

--- firstproject/firstproject/views.py
+++ firstproject/firstproject/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request, "home.html")
-    # return HttpResponse("jnbjhf")
+    name = ["Hridoy", "Badhon", "Sagor", "Bijoy"]
+    # contact = {"name": name}
+    return render(request, "home.html", {"name": name})
