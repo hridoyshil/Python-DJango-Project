7f8b12f4c7b2f459f4498f32d5f8bb7448ba84bc
Hridoy <hriodykumarshil12@gmail.com>
2023-09-22 23:10:53 +0600

    Create Django App

--- firstproject/firstproject/views.py
+++ firstproject/firstproject/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, "home.html")
+    # return HttpResponse("jnbjhf")
