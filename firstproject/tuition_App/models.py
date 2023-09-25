14577b336ea2c837cc26bf1f35a41531267900bd
Hridoy <hriodykumarshil12@gmail.com>
2023-09-26 00:12:57 +0600

    Image Field and save Converted size
    pip install Pillow
    
    https://www.youtube.com/watch?v=9Ojp7484V2A&list=PL4NIq30KvXLDf3a3DQXZyGv_BNYRYTXJS&index=17

--- firstproject/tuition_App/models.py
+++ firstproject/tuition_App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
-
+from PIL import Image
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -30,6 +31,15 @@ class Post(models.Model):
     category = models.CharField(max_length=100, choices=CATEGORY)
     created_at = models.DateTimeField(default=now)
     image = models.ImageField(upload_to="tuition/images")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
     def __str__(self):
         return self.title
