5ebc076004253484b80f31ed9f61df8d234b58d7
Hridoy <hriodykumarshil12@gmail.com>
2023-09-29 22:04:43 +0600

    Form Design | All Tasks With Form

--- firstproject/tuition_App/forms.py
+++ firstproject/tuition_App/forms.py
@@ -4,9 +4,45 @@ from .models import Contact, Post
 
 
 class ContactForm(forms.ModelForm):
+    # name = (
+    #     forms.CharField(
+    #         max_length=100,
+    #         widget=forms.TextInput(
+    #             attrs={"class": "form-control", "placeholder": "Enter Your Name"}
+    #         ),
+    #     ),
+    # )
+
     class Meta:
         model = Contact
         fields = "__all__"
+
+        # Form Design--MOdel Form
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter Your Name"}
+            ),
+            "phone": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter Your Phone"}
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Say something",
+                    "rows": 5,
+                }
+            ),
+        }
+        labels = {
+            "name": "Your Name",
+            "phone": "Your Phone Number",
+            "content": "Your Words",
+        }
+        help_texts = {
+            "name": "Your Name",
+            "phone": "Your Phone Number",
+            "content": "Your Words",
+        }
 
 
 # class PostForm(forms.ModelForm):
